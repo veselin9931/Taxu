39b6380f9601001aafa9e1fe89aa51e43256469b
veselin9931 <ves.georgiev99@gmail.com>
2021-01-11 17:49:39 +0200

    Rating model

--- TravelApp/TravelApp.Data/ApplicationUser.cs
+++ TravelApp/TravelApp.Data/ApplicationUser.cs
@@ -1,14 +1,30 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using TravelApp.Common.BaseModels;
+using TravelApp.Models;
 
 namespace TravelApp.Data
 {
     public class ApplicationUser : IdentityUser, IDeletableEntity
     {
         public string PasswordSalt { get; set; }
+
         public bool IsDeleted { get; set; }
 
         public DateTime? DeletedOn { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string CurrentLcatiion { get; set; }
+
+        public string LastLocation { get; set; }
+
+        public Driver Driver { get; set; }
+
+        public string DriverId { get; set; }
     }
 }
---
+++ TravelApp/TravelApp.Models/Rating.cs
@@ -0,0 +1,14 @@
+﻿using TravelApp.Common.BaseModels;
+using TravelApp.Data
+
+namespace TravelApp.Models
+{
+    public class Rating : BaseDeletableModel<string>
+    {
+        public string DriverId { get; set; }
+
+        public Driver Driver { get; set; }
+
+        public double Value { get; set; }
+    }
+}
