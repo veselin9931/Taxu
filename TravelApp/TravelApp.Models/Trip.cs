32f2ae0e8bb5ed162ac4a4a44313ff555d9f4bc6
veselin9931 <ves.georgiev99@gmail.com>
2021-01-11 14:30:57 +0200

    Car model

--- TravelApp/TravelApp.Models/Trip.cs
+++ TravelApp/TravelApp.Models/Trip.cs
@@ -4,6 +4,14 @@ namespace TravelApp.Models
 {
     public class Trip : BaseDeletableModel<string>, IAuditInfo
     {
+        public string CurrentLocation { get; set; }
 
+        public string Location { get; set; }
+
+        public string Destination { get; set; }
+
+        public string CarId { get; set; }
+
+        public Car Car { get; set; }
     }
 }
