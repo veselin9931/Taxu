ce6311c27d94e9cbd425807e449785ec635c1962
veselin9931 <ves.georgiev99@gmail.com>
2021-01-14 17:06:56 +0200

    I implement AccountController.

--- TravelApp/TravelApp/Controllers/AccountController.cs
+++ TravelApp/TravelApp/Controllers/AccountController.cs
@@ -1,8 +1,18 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using TravelApp.Infrastructure.ViewModels;
+using TravelApp.Models;
+using TravelApp.Services.Account;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,6 +22,19 @@ namespace TravelApp.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly IConfiguration configuration;
+        private readonly IAccountService userService;
+
+        public AccountController(
+                                 UserManager<ApplicationUser> userManager,
+                                 IConfiguration configuration,
+                                 IAccountService userService)
+        {
+            this.userManager = userManager;
+            this.configuration = configuration;
+            this.userService = userService;
+        }
         // GET: api/<AccountController>
         [HttpGet]
         public IEnumerable<string> Get()
@@ -28,8 +51,60 @@ namespace TravelApp.Controllers
 
         // POST api/<AccountController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] RegisterViewModel model)
         {
+            if (model == null || !this.ModelState.IsValid)
+            {
+                return this.BadRequest("Failed to register");
+            }
+
+            var user = new ApplicationUser()
+            {
+                Email = model.Username,
+                UserName = model.Username,
+            };
+
+            var result = await userService.Create(user, model.Password);
+
+            if (result != null)
+            {
+                return this.Ok();
+            }
+
+            return this.BadRequest("Failed to register");
+        }
+
+        // POST api/<AccountController>
+        [HttpPost("authenticate")]
+        [Route("/authenticate")]
+        public async Task<IActionResult> Authenticate([FromBody] LoginViewModel userDto)
+        {
+            var user = await userService.Authenticate(userDto.Username, userDto.Password);
+
+            if (user == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(configuration["JwtTokenValidation:Secret"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            // return basic user info (without password) and token to store client side
+            return Ok(new
+            {
+                Id = user.Id,
+                Username = user.UserName,
+                Token = tokenString
+            });
         }
 
         // PUT api/<AccountController>/5
