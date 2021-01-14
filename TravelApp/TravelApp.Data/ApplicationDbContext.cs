61f94bda77e7b08ee1e6910834e8b647df2a2d3f
veselin9931 <ves.georgiev99@gmail.com>
2021-01-14 16:56:19 +0200

    Register service is ready.

--- TravelApp/TravelApp.Data/ApplicationDbContext.cs
+++ TravelApp/TravelApp.Data/ApplicationDbContext.cs
@@ -1,12 +1,8 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using IdentityServer4.EntityFramework.Options;
-using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.Extensions.Options;
 using TravelApp.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
-using Microsoft.SqlServer.Dac.Model;
 
 namespace TravelApp.Data
 {
