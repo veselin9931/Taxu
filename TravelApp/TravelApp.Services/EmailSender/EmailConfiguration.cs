d5b442c366b8832610d2868982b4ea810bf81cd1
veselin9931 <ves.georgiev99@gmail.com>
2021-01-18 21:58:29 +0200

    EmailSender

---
+++ TravelApp/TravelApp.Services/EmailSender/EmailConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelApp.Services.EmailSender
+{
+    public class EmailConfiguration
+    {
+        public string From { get; set; }
+        public string SmtpServer { get; set; }
+        public int Port { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ TravelApp/TravelApp.Services/EmailSender/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelApp.Services.EmailSender
+{
+    public interface IEmailSender
+    {
+        void SendEmail(Message message);
+        Task SendEmailAsync(Message message);
+    }
+}
