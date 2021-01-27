91cd0dd4a464d48b5d6f7c1d0024cad97aca32e6
veselin9931 <ves.georgiev99@gmail.com>
2021-01-27 20:13:50 +0200

    Added new migration.

---
+++ TravelApp/TravelApp.Data/Migrations/20210127181314_SmallUpdate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TravelApp.Data.Migrations
+{
+    public partial class SmallUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDriver",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDriver",
+                table: "AspNetUsers");
+        }
+    }
+}
