56347af854bc619a2b3b4116c3f487190e648b66
veselin9931 <ves.georgiev99@gmail.com>
2021-01-27 18:43:08 +0200

    Added new Input and View Models.

--- TravelApp/TravelApp.Services/DriverService/IDriverService.cs
+++ TravelApp/TravelApp.Services/DriverService/IDriverService.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using TravelApp.Infrastructure.InputModels.DriverInput;
 using TravelApp.Infrastructure.ViewModels;
 
 namespace TravelApp.Services.DriverService
 {
     public interface IDriverService
     {
+        IEnumerable<DriverViewModel> CreateDriver(DriverInputModel driverInputModel);
+
         IEnumerable<DriverViewModel> GetAllDrivers();
     }
 }
