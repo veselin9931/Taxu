627eeecac0b861974467ccf46eda07154a6ae29a
veselin9931 <ves.georgiev99@gmail.com>
2021-01-28 17:35:43 +0200

    Implement seeder.

---
+++ TravelApp/TravelApp.Data/Seeding/ISeeder.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelApp.Data.Seeding
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public interface ISeeder
+    {
+        Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider);
+    }
+}
