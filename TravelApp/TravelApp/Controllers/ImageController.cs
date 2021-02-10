06dc4ab65d74fb3af5c4e3e74fba67c174da8f97
veselin9931 <ves.georgiev99@gmail.com>
2021-02-10 16:28:45 +0200

    Added image controller.

--- TravelApp/TravelApp/Controllers/ImageController.cs
+++ TravelApp/TravelApp/Controllers/ImageController.cs
@@ -3,45 +3,79 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TravelApp.Infrastructure.InputModels.ImageInput;
+using TravelApp.Services.ImageService;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TravelApp.Controllers
 {
+
     [Route("api/[controller]")]
     [ApiController]
     public class ImageController : ControllerBase
     {
-        // GET: api/<ImageController>
-        [HttpGet]
-        public IEnumerable<string> Get()
+        private readonly IImageService service;
+
+        public ImageController(IImageService service)
         {
-            return new string[] { "value1", "value2" };
+            this.service = service;
         }
 
-        // GET api/<ImageController>/5
+        // GET api/<imageController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<IActionResult> Get(string id)
         {
-            return "value";
+            var image = await this.service.GetImagelByIdAsync(id);
+
+            if (image != null)
+            {
+                return this.Ok(image);
+            }
+
+            return this.BadRequest($"Failed to load image with id={id} from db");
         }
 
         // POST api/<ImageController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] CreateImageInputModel input)
         {
+            input.File = (Microsoft.AspNetCore.Http.IFormFile)Request.Form.Files;
+
+            if (this.ModelState.IsValid)
+            {
+                try
+                {
+                    var result = await this.service.CreateImageAsync(input);
+
+                    if (result != "")
+                    {
+                        return this.Ok(result);
+                    }
+
+                }
+                catch (Exception e)
+                {
+
+                    return this.BadRequest(e.Message);
+                }
+            }
+
+            return this.BadRequest("Failed to create image");
         }
 
-        // PUT api/<ImageController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ImageController>/5
+        // DELETE api/<imageController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(string id)
         {
+            var image = await this.service.DeleteImageAsync(id);
+
+            if (image)
+            {
+                return this.Ok(image);
+            }
+
+            return this.BadRequest($"Failed to delete image.");
         }
     }
 }
