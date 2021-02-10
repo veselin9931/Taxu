19c16e44e31f5e54024015ede171b7a7556536d5
veselin9931 <ves.georgiev99@gmail.com>
2021-02-10 16:46:03 +0200

    I added Reports models.

---
+++ TravelApp/TravelApp.Models/Report.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using TravelApp.Common.BaseModels;
+
+namespace TravelApp.Models
+{
+    public class Report : BaseDeletableModel<string>
+    {
+        public Report()
+        {
+            this.Id = new Guid().ToString();
+            this.CreatedOn = DateTime.UtcNow;
+        }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public ReportType TypeId { get; set; }
+        public ReportType Type { get; set; }
+
+
+        public string ReporterId { get; set; }
+        public ApplicationUser Reporter { get; set; }
+
+        public string SuspectedDriverId { get; set; }
+        public Driver SuspectedDriver { get; set; }
+    }
+}
---
+++ TravelApp/TravelApp.Models/ReportType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TravelApp.Common.BaseModels;
+
+namespace TravelApp.Models
+{
+    public class ReportType : BaseModel<int>
+    {
+        public ReportType()
+        {
+            this.CreatedOn = DateTime.UtcNow;
+        }
+
+        public string Name { get; set; }
+    }
+}
