47d2523cc3686e95fd193dcc9518aa257ddb1d64
veselin9931 <ves.georgiev99@gmail.com>
2021-04-26 13:35:45 +0300

    I added errs for imageService.

---
+++ TravelApp/TravelApp.Common/ErrMsg/ImageErrs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelApp.Common.ErrMsg
+{
+    public static class ImageErrs
+    {
+        public const string InvalidId = "This imageID not exist!";
+
+        public const string InvalidModel = "Invalid image model. Try again!";
+    }
+}
