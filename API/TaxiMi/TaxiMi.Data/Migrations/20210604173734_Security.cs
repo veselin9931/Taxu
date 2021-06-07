47463ff8cf5fcdc6f7e25c6e81b9c0170f8c83e8
veselin9931 <ves.georgiev99@gmail.com>
2021-06-07 15:33:40 +0300

    Added new prop

---
+++ API/TaxiMi/TaxiMi.Data/Migrations/20210604173734_Security.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TaxiMi.Data.Migrations
+{
+    public partial class Security : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Role",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Token",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Token",
+                table: "AspNetUsers");
+        }
+    }
+}
