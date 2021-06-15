7a7f02226e031a5dc9e4fb037c52e33ca6b7b87e
marian122 <maleksiev12@gmail.com>
2021-06-15 12:38:41 +0300

    bvnn

--- TaxiMeAdminApp/src/environments/environment.prod.ts
+++ TaxiMeAdminApp/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://www.taximiapi.com.aspbg.net',
- signalRUrl: 'http://www.taximiapi.com.aspbg.net',
+  apiUrl: 'https://cyber.api.mycybercab.com',
+  signalRUrl: 'https://cyber.api.mycybercab.com',
 
 // apiUrl: 'https://localhost:44377',
 // signalRUrl: 'https://localhost:44377',
